3acccb8618d06a00dc30144356c7adcca3176dcc
Diego <dlogon@outlook.com>
2024-05-23 16:57:35 -0600

    add files

---
+++ draw.js
@@ -0,0 +1,162 @@
+const drawGrid = (canvas, ctx, tileSize, highlightNum) => {
+    for (let y = 0; y < canvas.width / tileSize; y++) {
+        for (let x = 0; x < canvas.height / tileSize; x++) {
+            const parity = (x + y) % 2;
+            const tileNum = x + canvas.width / tileSize * y;
+            const xx = x * tileSize;
+            const yy = y * tileSize;
+
+            if (tileNum === highlightNum) {
+                ctx.fillStyle = "#f0f";
+            }
+            else {
+                ctx.fillStyle = parity ? "#DBDBDB" : "#BBBBBB";
+            }
+            if (data[y][x] == 1) {
+                ctx.fillStyle = "#000";
+            }
+
+            ctx.fillRect(xx, yy, tileSize, tileSize);
+
+            ctx.fillText("", xx, yy);
+        }
+    }
+};
+
+const generateArray = (size) => {
+    // Crear una matriz bidimensional inicializada con ceros
+    let array = [];
+    for (let i = 0; i < size; i++) {
+        let row = [];
+        for (let j = 0; j < size; j++) {
+            row.push(0);
+        }
+        array.push(row);
+    }
+    return array;
+}
+
+// Función para mover elementos hacia arriba
+const moveUp = () => {
+    let size = data.length;
+    for (let i = 1; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            data[i - 1][j] = data[i][j];
+        }
+    }
+    for (let j = 0; j < size; j++) {
+        data[size - 1][j] = 0;
+    }
+    drawGrid(canvas, ctx, tileSize);
+}
+
+// Función para mover elementos hacia abajo
+const moveDown = () => {
+    let size = data.length;
+    for (let i = size - 2; i >= 0; i--) {
+        for (let j = 0; j < size; j++) {
+            data[i + 1][j] = data[i][j];
+        }
+    }
+    for (let j = 0; j < size; j++) {
+        data[0][j] = 0;
+    }
+    drawGrid(canvas, ctx, tileSize);
+}
+
+// Función para mover elementos hacia la izquierda
+const moveLeft = () => {
+    let size = data.length;
+    for (let i = 0; i < size; i++) {
+        for (let j = 1; j < size; j++) {
+            data[i][j - 1] = data[i][j];
+        }
+    }
+    for (let i = 0; i < size; i++) {
+        data[i][size - 1] = 0;
+    }
+    drawGrid(canvas, ctx, tileSize);
+
+}
+
+// Función para mover elementos hacia la derecha
+const moveRight = () => {
+    let size = data.length;
+    for (let i = 0; i < size; i++) {
+        for (let j = size - 2; j >= 0; j--) {
+            data[i][j + 1] = data[i][j];
+        }
+    }
+    for (let i = 0; i < size; i++) {
+        data[i][0] = 0;
+    }
+    drawGrid(canvas, ctx, tileSize);
+
+}
+
+function binaryRowToHex(row) {
+    // Convertir la fila binaria a una cadena de bits
+    let binaryString = row.join('');
+    // Convertir la cadena binaria a un número hexadecimal
+    let hexString = parseInt(binaryString, 2).toString(16).toUpperCase();
+    return hexString;
+}
+
+function convertArrayToHex() {
+    // Convertir cada fila de la matriz a hexadecimal
+    let hexArray = data.map(binaryRowToHex);
+
+    console.log(hexArray);
+    return hexArray;
+}
+
+const size = 8;
+const canvas = document.createElement("canvas");
+canvas.width = canvas.height = 600;
+const ctx = canvas.getContext("2d");
+
+ctx.font = "11px courier";
+ctx.textBaseline = "top";
+const tileSize = canvas.width / size;
+const status = document.createElement("pre");
+let lastTile = -1;
+
+const data = generateArray(8);
+
+drawGrid(canvas, ctx, tileSize);
+document.body.style.display = "flex";
+document.body.style.alignItems = "flex-start";
+document.body.appendChild(canvas);
+document.body.appendChild(status);
+
+canvas.addEventListener("mousemove", evt => {
+    evt.target.style.cursor = "pointer";
+    const tileX = ~~(evt.offsetX / tileSize);
+    const tileY = ~~(evt.offsetY / tileSize);
+    const tileNum = tileX + canvas.width / tileSize * tileY;
+
+    if (tileNum !== lastTile) {
+        lastTile = tileNum;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawGrid(canvas, ctx, tileSize, tileNum);
+    }
+
+    status.innerText = `  mouse coords: {${evt.offsetX}, ${evt.offsetX}}
+    tile coords : {${tileX}, ${tileY}}
+    tile number : ${tileNum}`;
+});
+
+canvas.addEventListener("click", evt => {
+    status.innerText += "\n  [clicked]";
+    const tileX = ~~(evt.offsetX / tileSize);
+    const tileY = ~~(evt.offsetY / tileSize);
+    data[tileY][tileX] = data[tileY][tileX] == 0 ? 1 : 0;
+
+    console.log(data);
+});
+
+canvas.addEventListener("mouseout", event => {
+    drawGrid(canvas, ctx, tileSize);
+    status.innerText = "";
+    lastTile = -1;
+});
