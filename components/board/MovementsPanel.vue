609af885101c9bbd7095709527cd7972ef444701
Diego <dlogon@outlook.com>
2024-11-13 22:00:28 -0700

    style: add border

--- components/board/MovementsPanel.vue
+++ components/board/MovementsPanel.vue
@@ -1,5 +1,7 @@
 <template>
-  <h3 class="text-center text-lg border-spacing-1 border p-2 m-4">
+  <h3
+    class="text-center text-lg border-spacing-1 border p-2 m-4 dark:border-gray-200 border-black"
+  >
     Move highlight cells
   </h3>
   <div class="grid grid-cols-3 text-center text-xl p-10 m-2">
