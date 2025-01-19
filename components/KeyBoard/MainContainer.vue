1c4e479ef3575bf68046c8c02827fbdd7e97ae93
Diego <dlogon@outlook.com>
2025-01-19 16:05:45 -0600

    feat: add clear button and enhance key press handling for improved functionality

--- components/KeyBoard/MainContainer.vue
+++ components/KeyBoard/MainContainer.vue
@@ -22,10 +22,13 @@
           @key-button-clicked="handleKeyButtonClick"
         />
       </div>
+      <div class="flex justify-center p-3 col-span-4">
+        <MyUIButton @click="clearAll" class="text-center"> CLEAR </MyUIButton>
+      </div>
     </div>
     <div class="w-full flex flex-col">
       <div class="w-full">
-        {{ result }}
+        {{ currentMemoryLocation }}
         <SharedResultString :result="result" />
       </div>
       <div>
@@ -37,27 +40,31 @@
 
 <script setup lang="ts">
 import { segmentHex } from "../../constants/keyHexCodes";
-import { ref } from "vue";
 
-import { onMounted, onUnmounted } from "vue";
+//Cookies
+const currentPhrase = useCookie<string>("currentPhrase", { default: () => "" });
+const result = useCookie<string>("result", {
+  default: () => "",
+});
 
-function handleKeyPress(e: KeyboardEvent) {
-  console.log("Key pressed:", e.key);
-}
+const currentMemoryLocation = useCookie<number>("currentMemoryLocation", {
+  default: () => 0,
+});
 
+const numberOfLettersInLine = useCookie<number>("numberOfLettersInLine", {
+  default: () => 0,
+});
+
+//Events
 onMounted(() => {
-  window.addEventListener("keypress", handleKeyPress);
+  window.addEventListener("keydown", handleKeyPress);
+
+  // currentMemoryLocation.value = 0;
 });
 
 onUnmounted(() => {
-  window.removeEventListener("keypress", handleKeyPress);
+  window.removeEventListener("keydown", handleKeyPress);
 });
-
-var currentMemoryLocation = 0;
-var numberOfLettersInLine = 0; // TODO CHANGE THIS, with the option of delete the last letter
-const result = ref("");
-const currentPhrase = ref("");
-var readableResult = ""; // TODO unused variable inherited from the original code
 
 const handleKeyButtonClick = (keyPusehd: {
   keyLetter: string;
@@ -69,23 +76,78 @@ const handleKeyButtonClick = (keyPusehd: {
   handleHex(keyHex);
 };
 
+//Functions
+/**
+ * Handle key press event
+ * @param e KeyboardEvent
+ */
+const handleKeyPress = (e: KeyboardEvent) => {
+  if (e.key === "Backspace" || e.key === "Delete") {
+    onPressDelete();
+    return;
+  }
+
+  const key = e.key.toUpperCase();
+  if (!/^[A-Z ]$/.test(key)) return;
+  const segment = segmentHex[key] ?? "00";
+  if (segment) {
+    currentPhrase.value = currentPhrase.value + key;
+    handleHex(segment);
+  }
+};
+
+const clearAll = () => {
+  currentPhrase.value = "";
+  result.value = "";
+  currentMemoryLocation.value = 0;
+  numberOfLettersInLine.value = 0;
+};
+
+/**
+ * Handle delete button press
+ * TODO check if is first memory location
+ */
+const onPressDelete = () => {
+  const currentPhraseValue = currentPhrase.value;
+  const resultValue = result.value;
+
+  if (result.value.length > 0) {
+    result.value = resultValue.slice(0, -2);
+  }
+  if (currentPhrase.value.length > 0) {
+    currentPhrase.value = currentPhraseValue.slice(0, -1);
+  }
+  const lastString = result.value.split("\n").pop() || "";
+
+  if (lastString.length == 9) {
+    currentMemoryLocation.value = currentMemoryLocation.value - 2 || 0;
+    result.value = result.value.slice(0, -9);
+    numberOfLettersInLine.value = 0;
+  }
+
+  if (currentPhrase.value.length == 0) {
+    clearAll();
+  }
+};
+
+// TODO CHANGE THIS, with the option of delete the last letter
+
 const handleHex = (keyHex: string) => {
   const stringResult: string = keyHex;
 
   if (!stringResult) return;
 
-  numberOfLettersInLine++;
-  if (numberOfLettersInLine == 1) {
-    const location = generateLocationString(currentMemoryLocation);
+  numberOfLettersInLine.value++;
+  if (numberOfLettersInLine.value == 1) {
+    debugger;
+    const location = generateLocationString(currentMemoryLocation.value);
     result.value = result.value + location + stringResult;
-  } else if (numberOfLettersInLine == 32) {
+  } else if (numberOfLettersInLine.value == 32) {
     result.value = result.value + stringResult + "\n";
-    numberOfLettersInLine = 0;
-    currentMemoryLocation += 2;
+    numberOfLettersInLine.value = 0;
+    currentMemoryLocation.value += 2;
   } else {
     result.value = result.value + stringResult;
   }
-
-  console.log(result);
 };
 </script>
